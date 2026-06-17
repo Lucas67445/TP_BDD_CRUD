14543c5b8d90e7d2bca8a872c5da33d0585f4688
Lucas Hartmann <Gaame@BOOK-BLQAKQ76SF>
2026-06-17 13:25:43 +0200

    Modification du fichier connexion.php

--- connexion.php
+++ connexion.php
@@ -6,7 +6,6 @@ $password = 'root';
 
 try {
     $pdo = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8", $user, $password);
-    echo "Connexion réussie !";
 } catch (PDOException $e) {
     echo "Erreur : " . $e->getMessage();
 }
