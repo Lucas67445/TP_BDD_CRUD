8516dcc930f120c2a0dbb38900d8219ddb9e5f83
Lucas Hartmann <Gaame@BOOK-BLQAKQ76SF>
2026-06-19 13:56:22 +0200

    Modification du fichier index.php

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>TP BDD CRUD</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion de collection</title>
 </head>
 <body>
-    <h1>TP BDD CRUD</h1>
-
-    <ul>
-        <li><a href="elements.php">Éléments</a></li>
-        <li><a href="collections.php">Collections</a></li>
-        <li><a href="emprunts.php">Emprunts</a></li>
-        <li><a href="ajout_element.php">Ajout Elements</a></li>
-        <li><a href="ajout_collection.php">Ajout Collections</a></li>
-    </ul>
+    
 </body>
 </html>
